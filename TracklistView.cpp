d4e1bc115eeb28d572afac0cec6eda9604dbfda1
Jan Krassnigg <Jan@Krassnigg.de>
2019-04-20 23:58:03 +0200

    Fixes rendering of the song rating widget

--- TracklistView.cpp
+++ TracklistView.cpp
@@ -1,16 +1,17 @@
 #include "TrackListView.h"
-#include <QKeyEvent>
-#include <QDrag>
-#include <QPainter>
-#include <QMouseEvent>
 #include "MusicLibrary.h"
+#include <QDrag>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QPainter>
 
 TrackListView::TrackListView(QWidget* parent)
-  : QTreeView(parent)
+    : QTreeView(parent)
 {
   setDragEnabled(true);
 
   setItemDelegateForColumn(0, new RatingItemDelegate(this));
+  setEditTriggers(QAbstractItemView::EditTrigger::NoEditTriggers);
 
   setSortingEnabled(true);
 }
@@ -34,23 +35,24 @@ void TrackListView::startDrag(Qt::DropActions supportedActions)
 {
   //Q_D(QAbstractItemView);
   QModelIndexList indexes = selectedIndexes();
-  if (indexes.count() > 0) {
-    QMimeData *data = model()->mimeData(indexes);
+  if (indexes.count() > 0)
+  {
+    QMimeData* data = model()->mimeData(indexes);
     if (!data)
       return;
     QRect rect;
     //QPixmap pixmap = d->renderToPixmap(indexes, &rect);
     rect.adjust(horizontalOffset(), verticalOffset(), 0, 0);
-    QDrag *drag = new QDrag(this);
+    QDrag* drag = new QDrag(this);
     //drag->setPixmap(pixmap);
     drag->setMimeData(data);
     //drag->setHotSpot(d->pressedPosition - rect.topLeft());
     //drag->setHotSpot(QPoint(pixmap.width() / 2, pixmap.height() / 2));
     Qt::DropAction defaultDropAction = Qt::CopyAction;
     //if (d->defaultDropAction != Qt::IgnoreAction && (supportedActions & d->defaultDropAction))
-      //defaultDropAction = d->defaultDropAction;
+    //defaultDropAction = d->defaultDropAction;
     //else if (supportedActions & Qt::CopyAction && dragDropMode() != QAbstractItemView::InternalMove)
-      //defaultDropAction = Qt::CopyAction;
+    //defaultDropAction = Qt::CopyAction;
 
     if (drag->exec(supportedActions, defaultDropAction) == Qt::MoveAction)
     {
@@ -64,13 +66,13 @@ void TrackListView::startDrag(Qt::DropActions supportedActions)
 }
 
 RatingItemDelegate::RatingItemDelegate(QObject* parent)
-  : QItemDelegate(parent)
+    : QItemDelegate(parent)
 {
   m_Pixmap = QIcon(":/icons/icons/star.png").pixmap(16);
   m_PixmapGrey = QIcon(":/icons/icons/star_grey.png").pixmap(16);
 }
 
-void RatingItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+void RatingItemDelegate::paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
   if (option.state & QStyle::State_Selected)
     painter->fillRect(option.rect, option.palette.highlight());
@@ -81,7 +83,8 @@ void RatingItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &op
 
   QRect r = option.rect;
   r.setHeight(16);
-  r.setLeft(10);
+  r.setLeft(20);
+  r.setRight(20 + 5 * 16);
 
   painter->setClipRect(r);
 
@@ -112,23 +115,25 @@ bool RatingItemDelegate::editorEvent(QEvent* event, QAbstractItemModel* model, c
     QMouseEvent* mouseEvent = (QMouseEvent*)event;
 
     int x = mouseEvent->pos().x();
-    int newRating = (x - 10) / 16 + 1;
+    int newRating = (x - 20) / 16 + 1;
 
     if (newRating > 5)
       return false;
 
-    if (x < 10)
+    if (x < 20)
       newRating = 0;
 
     QString guid = index.data(Qt::UserRole + 1).toString();
 
     MusicLibrary::GetSingleton()->UpdateSongRating(guid, newRating, true);
+
+    model->dataChanged(index, index);
   }
 
   return false;
 }
 
-QSize RatingItemDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
+QSize RatingItemDelegate::sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
-  return QSize(16 * 5, 16);
+  return QSize(16 * 5 + 20, 16);
 }
